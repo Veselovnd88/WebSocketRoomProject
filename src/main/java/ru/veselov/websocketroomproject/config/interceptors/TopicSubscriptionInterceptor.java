0f2f611fef36ba82b062aaa13aa61a1dd80d86d5
Veselov Nikolay <veselovnd@gmail.com>
2023-03-17 17:00:20 +0300

    ready and tested

--- src/main/java/ru/veselov/websocketroomproject/config/interceptors/TopicSubscriptionInterceptor.java
+++ src/main/java/ru/veselov/websocketroomproject/config/interceptors/TopicSubscriptionInterceptor.java
@@ -1,7 +1,6 @@
 package ru.veselov.websocketroomproject.config.interceptors;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessagingException;
@@ -9,6 +8,10 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 
+/**
+ * Interceptor validates all Subscription commands for null destination
+ * or not correct prefix
+ */
 @Slf4j
 public class TopicSubscriptionInterceptor implements ChannelInterceptor {
     private final String destinationPrefix;
@@ -21,7 +24,7 @@ public class TopicSubscriptionInterceptor implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         if (StompCommand.SUBSCRIBE.equals(accessor.getCommand()) && (!validateHeaders(accessor))) {
-            throw new MessagingException("Destination cannot be null or start with prefix [/topic]");
+            throw new MessagingException("Destination cannot be null and should start with prefix [/topic]");
         }
         return message;
     }
--- src/test/java/ru/veselov/websocketroomproject/config/interceptors/TopicSubscriptionInterceptorTest.java
+++ src/test/java/ru/veselov/websocketroomproject/config/interceptors/TopicSubscriptionInterceptorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
@@ -18,12 +17,14 @@ import java.util.Map;
 
 @SpringBootTest
 class TopicSubscriptionInterceptorTest {
+    @Value("${socket.dest-prefix}")
+    private String destinationPrefix;
 
     @Test
     void shouldReturnMessage() {
-        ChannelInterceptor interceptor = new TopicSubscriptionInterceptor("/topic");
+        ChannelInterceptor interceptor = new TopicSubscriptionInterceptor(destinationPrefix);
         MessageChannel channel = Mockito.mock(MessageChannel.class);
-        Message<byte[]> message = Mockito.mock(Message.class);
+        Message<?> message = Mockito.mock(Message.class);
         Map<String, Object> headers = new HashMap<>();
         headers.put("stompCommand", StompCommand.SUBSCRIBE);
         headers.put("simpDestination", "/topic/users/5");
@@ -36,15 +37,26 @@ class TopicSubscriptionInterceptorTest {
     void shouldThrowMessagingExceptionIfDestinationIsNull() {
         ChannelInterceptor interceptor = new TopicSubscriptionInterceptor("/topic");
         MessageChannel channel = Mockito.mock(MessageChannel.class);
-        Message<byte[]> message = Mockito.mock(Message.class);
+        Message<?> message = Mockito.mock(Message.class);
         Map<String, Object> headers = new HashMap<>();
         headers.put("stompCommand", StompCommand.SUBSCRIBE);
-        headers.put("simpDestination", null);
         headers.put("simpSessionId", "test");
         Mockito.when(message.getHeaders()).thenReturn(new MessageHeaders(headers));
 
         Assertions.assertThatThrownBy(() -> interceptor.preSend(message, channel)).isInstanceOf(MessagingException.class);
     }
 
+    @Test
+    void shouldThrowMessagingExceptionIfDestinationIsNotCorrect() {
+        ChannelInterceptor interceptor = new TopicSubscriptionInterceptor("/topic");
+        MessageChannel channel = Mockito.mock(MessageChannel.class);
+        Message<?> message = Mockito.mock(Message.class);
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("stompCommand", StompCommand.SUBSCRIBE);
+        headers.put("simpSessionId", "test");
+        headers.put("simpDestination", "/tRopic/users/5");
+        Mockito.when(message.getHeaders()).thenReturn(new MessageHeaders(headers));
 
+        Assertions.assertThatThrownBy(() -> interceptor.preSend(message, channel)).isInstanceOf(MessagingException.class);
+    }
 }
