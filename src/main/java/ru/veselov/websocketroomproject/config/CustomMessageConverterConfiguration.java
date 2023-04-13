93588ca22fd7da7105ac7e3200a7ba4f798cb25f
Veselov Nikolay <veselovnd@gmail.com>
2023-04-13 12:31:51 +0300

    used default builder

--- src/main/java/ru/veselov/websocketroomproject/config/CustomMessageConverterConfiguration.java
+++ src/main/java/ru/veselov/websocketroomproject/config/CustomMessageConverterConfiguration.java
@@ -1,25 +1,22 @@
 package ru.veselov.websocketroomproject.config;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 
 @Configuration
-public class ConverterConfiguration {
-    @Bean
-    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.modules(new JavaTimeModule());
-        return builder;
-    }
+@RequiredArgsConstructor
+public class CustomMessageConverterConfiguration {
+    private final Jackson2ObjectMapperBuilder builder;
 
     @Bean
     public MappingJackson2MessageConverter messageConverter() {
+        builder.modules(new JavaTimeModule());
         MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-        messageConverter.setObjectMapper(objectMapperBuilder().build());
+        messageConverter.setObjectMapper(builder.build());
         return messageConverter;
     }
 
