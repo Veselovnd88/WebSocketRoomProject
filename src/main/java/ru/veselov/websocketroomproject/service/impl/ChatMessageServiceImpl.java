ba432fd8a1fbe06e4fe684da07bc588b8af1e35f
Veselov Nikolay <veselovnd@gmail.com>
2023-04-14 16:29:44 +0300

    ChatMessageService

--- src/main/java/ru/veselov/websocketroomproject/service/impl/ChatMessageServiceImpl.java
+++ src/main/java/ru/veselov/websocketroomproject/service/impl/ChatMessageServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.veselov.websocketroomproject.dto.ReceivedChatMessage;
 import ru.veselov.websocketroomproject.dto.SendChatMessage;
