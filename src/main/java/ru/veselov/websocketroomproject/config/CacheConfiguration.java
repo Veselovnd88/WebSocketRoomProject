433c3c0f23134e5932038bd6fbbd67269e8a39f6
Veselov Nikolay <veselovnd@gmail.com>
2023-04-14 17:16:22 +0300

    starting configure redis

---
+++ src/main/java/ru/veselov/websocketroomproject/config/CacheConfiguration.java
@@ -0,0 +1,26 @@
+package ru.veselov.websocketroomproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfiguration {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(120))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer()
+                ));
+    }
+
+
+}
