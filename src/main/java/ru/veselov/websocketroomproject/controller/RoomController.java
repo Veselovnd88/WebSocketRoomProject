32dcb42e15067c2629484e2e658f4210d15918f9
Veselov Nikolay <veselovnd@gmail.com>
2023-04-19 18:39:08 +0300

    making videocontroller

--- src/main/java/ru/veselov/websocketroomproject/controller/RoomController.java
+++ src/main/java/ru/veselov/websocketroomproject/controller/RoomController.java
@@ -3,8 +3,11 @@ package ru.veselov.websocketroomproject.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -14,9 +17,13 @@ public class RoomController {
     private final StreamingService service;
 
 
-    @GetMapping(value = "/room/{roomId}", produces = "video/mp4")
+    @GetMapping(value = "/room/{roomId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public Mono<Resource> getVideos(@PathVariable String roomId, @RequestHeader("Range") String range) {
         System.out.println("range in bytes() : " + range);
-        return service.getVideo(roomId);
+        try {
+            return service.getVideo(roomId);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ru/veselov/websocketroomproject/controller/StreamingService.java
+++ src/main/java/ru/veselov/websocketroomproject/controller/StreamingService.java
@@ -2,10 +2,16 @@ package ru.veselov.websocketroomproject.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +22,16 @@ public class StreamingService {
     private final ResourceLoader resourceLoader;
 
 
-    public Mono<Resource> getVideo(String title) {
-        return Mono.fromSupplier(() -> resourceLoader.
-                getResource(String.format(FORMAT, title)));
+    public Mono<Resource> getVideo(String title) throws IOException {
+        Resource resource = resourceLoader.getResource(String.format(FORMAT, title));
+        byte[] contentAsByteArray = resource.getContentAsByteArray();
+        UrlResource url = new UrlResource("https://www.youtube.com/watch?v=lJxjTLU9pGs");
+        InputStream inputStream = url.getInputStream();
+        byte[] bytes = inputStream.readAllBytes();
+
+
+
+        Resource byteArrayResource = new ByteArrayResource(bytes);
+        return Mono.just(byteArrayResource);
     }
 }
