62fb10e5c7477764760843f9f8c218ed79714155
Veselov Nikolay <veselovnd@gmail.com>
2023-05-06 18:48:15 +0300

    test for adding settings

--- src/test/java/ru/veselov/websocketroomproject/controller/RoomControllerTest.java
+++ src/test/java/ru/veselov/websocketroomproject/controller/RoomControllerTest.java
@@ -10,7 +10,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import ru.veselov.websocketroomproject.TestConstants;
+import ru.veselov.websocketroomproject.dto.RoomSettingsDTO;
 import ru.veselov.websocketroomproject.model.Room;
 import ru.veselov.websocketroomproject.repository.RoomRepository;
 import ru.veselov.websocketroomproject.service.RoomService;
@@ -39,8 +42,7 @@ class RoomControllerTest {
                         uriBuilder -> uriBuilder.path("api").path("/room").path("/" + ROOM_ID).build())
                 .headers(headers -> headers.add(TestConstants.AUTH_HEADER, TestConstants.BEARER_JWT))
                 .exchange().expectStatus().isOk().returnResult(Room.class);
-        Mockito.verify(roomService, Mockito.times(1))
-                .getRoomById(ROOM_ID, null);
+        Mockito.verify(roomService, Mockito.times(1)).getRoomById(ROOM_ID, null);
     }
 
     @Test
@@ -50,15 +52,23 @@ class RoomControllerTest {
                                 .queryParam("token", "secret").build())
                 .headers(headers -> headers.add(TestConstants.AUTH_HEADER, TestConstants.BEARER_JWT))
                 .exchange().expectStatus().isOk().returnResult(Room.class);
-        Mockito.verify(roomService, Mockito.times(1))
-                .getRoomById(ROOM_ID, "secret");
+        Mockito.verify(roomService, Mockito.times(1)).getRoomById(ROOM_ID, "secret");
     }
 
     @Test
     void shouldCallRoomServiceAndReturnACCEPTEDStatus() {
+        RoomSettingsDTO roomSettingsDTO = RoomSettingsDTO.builder().roomName("newName").playerType("Youtube").build();
         webTestClient.put().uri(
-                uriBuilder -> uriBuilder.path("api").path("/room").path("/"+ROOM_ID).build()
+                        uriBuilder -> uriBuilder.path("api").path("/room").path("/" + ROOM_ID).build())
+                .headers(headers -> headers.add(TestConstants.AUTH_HEADER, TestConstants.BEARER_JWT))
+                .bodyValue(roomSettingsDTO)
+                .exchange().expectStatus().isAccepted().returnResult(Room.class);
+        Mockito.verify(roomService, Mockito.times(1))
+                .changeSettings(
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.any(RoomSettingsDTO.class),
+                        ArgumentMatchers.any(Principal.class)
+                );
     }
-
 
 }
